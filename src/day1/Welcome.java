0554a18cc880f9bb1c4d5147bd40458598948853
Manoj <manojid28@gmail.com>
2021-11-23 22:33:33 -0600

    Message Changed

--- src/day1/Welcome.java
+++ src/day1/Welcome.java
@@ -2,7 +2,7 @@ package day1;
 
 public class Welcome {
     public static void main (String[] args) {
-        String greeting = "Welcome to Core Java";
+        String greeting = "Welcome to Core Java Training";
         System.out.println(greeting);
         for (int i = 0; i< greeting.length();i++ ) {
 
