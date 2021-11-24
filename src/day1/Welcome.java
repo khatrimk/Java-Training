900d6a840dcc303f71c9c43d4327c7cd8987abb6
Manoj <manojid28@gmail.com>
2021-11-23 23:12:15 -0600

    Add development on greeting

--- src/day1/Welcome.java
+++ src/day1/Welcome.java
@@ -2,7 +2,7 @@ package day1;
 
 public class Welcome {
     public static void main (String[] args) {
-        String greeting = "Welcome to Core Java Training 2021";
+        String greeting = "Welcome to Core Java Training 2021 is on development";
         System.out.println(greeting);
         for (int i = 0; i< greeting.length();i++ ) {
 
